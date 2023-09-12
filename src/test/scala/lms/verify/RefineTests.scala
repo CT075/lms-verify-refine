80d191eccc14fc9ea5b524fe5c8d956508244ec0
Cameron Wong <cwong@g.harvard.edu>
2023-09-12 17:19:09 -0400

    very basic requires/ensures cleanup
    Signed-off-by: Cameron Wong <cwong@g.harvard.edu>

--- src/test/scala/lms/verify/RefineTests.scala
+++ src/test/scala/lms/verify/RefineTests.scala
@@ -4,40 +4,88 @@ class RefineTestBasic extends TestSuite {
   val under = "refine"
 
   trait Refines extends Dsl with DataOps {
-    trait Prop[A] {
-      def reify(x: Rep[A]): Rep[Boolean]
+    sealed trait Proof[P] {
+      def emit(): Rep[Unit]
     }
 
-    trait Proof[A, P <: Prop[A]] {}
-
-    case class Refined[A, P <: Prop[A]](x: Rep[A], proof: Proof[A,P])
-
-    def unsafeProve[A, P <: Prop[A]]: Proof[A,P] = new Proof[A,P] {}
-
-    def requires_[A, P <: Prop[A]](x: Rep[A], p: P) = {
-      requires(p.reify(x))
-      Refined(x, unsafeProve[A,P])
+    def unsafeAssert[P](cond: Rep[Boolean]): Proof[P] = new Proof[P] {
+      def emit() = _assert(cond)
     }
 
-    def contractsOfRefines[A:Typ, PA <: Prop[A], B:Typ, PB <: Prop[B]]
-      (f: Refined[A, PA] => Refined[B, PB])
-      (implicit pa: PA, pb: PB): Rep[A] => Rep[B] = { x =>
-        val rx = requires_[A, PA](x, pa)
-        val y = f(rx).x
-        ensures{result: Rep[B] => pb.reify(result)}
-        y
+    trait Predicate[A] {
+      def verify(x: Rep[A]): Rep[Boolean]
+    }
+
+    case class TRUE[A]() extends Predicate[A] {
+      def verify(x: Rep[A]): Rep[Boolean] = {
+        unit(true)
+      }
+    }
+
+    def _true[A](): Proof[TRUE[A]] = new Proof[TRUE[A]] {
+      def emit() = {}
+    }
+
+    case class Refined[A: Manifest, P <: Predicate[A]](x: Rep[A], proof: Proof[P]) {
+      def unwrap(): Rep[A] = {
+        proof.emit()
+        x
+      }
+    }
+
+    def check[A: Manifest, P <: Predicate[A], B](
+      x: Rep[A],
+      _then: Refined[A, P] => Rep[B],
+      _else: => Rep[B]
+    )(implicit ev: Typ[B], pred: P) : Rep[B] = {
+      val cond: Rep[Boolean] = pred.verify(x)
+      if (cond) _then(Refined(x, unsafeAssert(cond))) else _else
+    }
+
+    // It'd be nice to do this implicitly
+    def requireAndEnsure[
+      A: Manifest,
+      B: Manifest,
+      Requires <: Predicate[A],
+      Ensures <: Predicate[B]
+    ](
+      f: Refined[A, Requires] => Refined[B, Ensures]
+    )(x: Rep[A])
+    (implicit precond: Requires, postcond: Ensures, evB: Typ[B]): Rep[B] = {
+      requires(precond.verify(x))
+      ensures(postcond.verify)
+
+      f(Refined(x, unsafeAssert(precond.verify(x)))).unwrap()
     }
   }
 
   test("1") {
     trait Ex1 extends Refines {
-      implicit object P extends Prop[Int] {
-        def reify(x: Rep[Int]) = x < 10
+      case class LessThan10() extends Predicate[Int] {
+        def verify(x: Rep[Int]) = x < 10
       }
 
-      toplevel("id",
-        // TODO: make this implicit
-        contractsOfRefines[Int, P.type, Int, P.type]{ (x: Refined[Int, P.type]) => x }
+      implicit val lt10 = LessThan10()
+      implicit val theTrue = TRUE[Int]()
+
+      def max9(x: Refined[Int, TRUE[Int]]): Refined[Int, LessThan10] = {
+        val result =
+          check[Int, LessThan10, Int](
+            x.unwrap(),
+            (t: Refined[Int, LessThan10]) => t.unwrap(),
+            unit(9)
+          )
+
+        Refined(
+          result,
+          unsafeAssert(result < 10)
+        )
+      }
+
+      toplevel("foo",
+        requireAndEnsure[Int, Int, TRUE[Int], LessThan10](
+          max9
+        )_
       )
     }
 
